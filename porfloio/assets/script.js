49d021c2049fe2e6c11b8ebf3a3a32a756745f7c
Borbely Tibor <136886437+TGamerTibor@users.noreply.github.com>
2023-07-30 22:01:18 +0300

    New animation and center the landig page

--- porfloio/assets/script.js
+++ porfloio/assets/script.js
@@ -25,3 +25,19 @@ function close() {
 }
 
 
+
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+
+  });
+});
+
+const hiddenElements =document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
